47bbb46b0cc4b226e04a0557fff98db046c0826f
Rafael Borges <rafaelborges.av@gmail.com>
2022-05-04 17:28:42 -0300

    aula 2

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = 'Rafael';
+let idade = '24';
+let comidaFavorita;
+
+comidaFavorita = 'lasanha';
+
+let carro, pet, jogo, pc, mae;
---
+++ tipoVariaveis.js
@@ -0,0 +1,15 @@
+let frase = 'cachorro idiota'
+
+let idade = '24'
+
+let age = 24
+
+let nome = 'rafael '
+let sobrenome = 'borges'
+let nomeCompleto = nome + sobrenome
+
+console.log(nomeCompleto)
+
+let frase2 = "it's time"
+
+console.log(typeof nome)
