bbffa419cba9048e761ed5b2553952377813524b
Rafael Borges <rafaelborges.av@gmail.com>
2022-06-11 15:35:57 -0300

    meu deus

---
+++ truth.js
@@ -0,0 +1,22 @@
+// function verificarverdadeiro(dado) {
+//   return !!dado;
+// }
+
+// verificarverdadeiro("rafael")
+
+// function areaquadrado(perimetro) {
+//   return perimetro * 4;
+// }
+
+// function nomeCompleto(nome, sobrenome) {
+//   return nome + ' ' + sobrenome;
+// }
+
+function epar(i) {
+  let modulo = i % 2;
+  if (modulo == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
