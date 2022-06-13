9868318b87f6928ba1c615f8e4dbd1c95f197785
Rafael Borges <rafaelborges.av@gmail.com>
2022-06-12 21:11:36 -0300

    update

--- truth.js
+++ truth.js
@@ -12,11 +12,31 @@
 //   return nome + ' ' + sobrenome;
 // }
 
-function epar(i) {
-  let modulo = i % 2;
-  if (modulo == 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function epar(i) {
+//   let modulo = i % 2;
+//   if (modulo == 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// addEventListener('click', function () {
+//   console.log('Rafael Borges');
+// });
+
+// var totalPaises = 193;
+// function precisoVisitar(paisesVisitados) {
+//   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+// }
+
+// function jaVisitei(paisesVisitados) {
+//   return `Já visitei ${paisesVisitados} do total de ${totalPaises} paises`;
+// }
+
+// precisoVisitar(20);
+// jaVisitei(20);
+
+// function valorReal(i) {
+//   return Math.round(i / 7);
+// }
