1bcac772e9806ed87fd1e446a90452f31db3ffbf
Rafael Borges <rafaelborges.av@gmail.com>
2022-06-17 15:13:20 -0300

    tudo é objeto

---
+++ tudoObjeto.js
@@ -0,0 +1,19 @@
+var numero = 1.4;
+
+numeroAjustado = numero.toFixed();
+
+var btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function () {
+  console.log('clicou');
+});
+
+btn.className
+btn.scrollTo
+btn.localName
+
+var nome = 'rafael'
+
+nome.length
+nome.split
+nome.toUpperCase
